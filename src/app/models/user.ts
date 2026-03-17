baf185878b1df1a8afd405204071f015f2cecd27
Stamen Stoychev <SStoychev@infragistics.com>
2026-03-17 09:16:33 +0200

    feat(*): redo app setup in react / asp.core 10

--- src/app/models/user.ts
+++
@@ -1,8 +0,0 @@
-﻿export class User {
-	id: string;
-	userName: string;
-	password: string;
-	firstName: string;
-	lastName: string;
-	token?: string;
-}
--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-	production: true,
-	apiUrl: 'https://myvideogamelist.net',
-	GOOGLE_CLIENT_ID: '131073576889-3cnd1i84ja5us2d0q0snegamiasn357d.apps.googleusercontent.com'
-};
